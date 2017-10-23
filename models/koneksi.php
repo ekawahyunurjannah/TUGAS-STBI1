46834f6ab9f2f5f41a12ed575ff6f5f5120d8640
Your Name <you@example.com>
2017-10-23 18:17:06 +0700

    [ADD]update configuration db

--- models/koneksi.php
+++ models/koneksi.php
@@ -1,8 +1,8 @@
 <?php
-	$user = "root";
-	$pass = "";
-	$db = "stbi";
-	$host = "localhost";
+	$user = "learnand_admin";
+	$pass = "@hayukalbu";
+	$db = "learnand_stbi";
+	$host = "jkt09.dewaweb.com:2083";
 	$konek = mysql_connect($host, $user, $pass);
 	mysql_select_db($db, $konek);
 ?>
